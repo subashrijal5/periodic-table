78ad019fbcfa0e80dd7143f5d9c8bfc998584929
subash rijal <subashrijal5@gmail.com>
2024-03-11 13:54:51 +0000

    feat: adds query to find element by argument

--- element.sh
+++ element.sh
@@ -1,7 +1,8 @@
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ $1 ]]
 then
-
+ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number='$1' OR symbol='$1' OR name='$1'")
+echo $ELEMENT
 else
 echo "Please provide an element as an argument."
 fi
