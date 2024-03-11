577464fec88d2c57df4cd7a0d7dab54455537fcf
subash rijal <subashrijal5@gmail.com>
2024-03-11 14:00:51 +0000

    feat: display message if no element found

--- element.sh
+++ element.sh
@@ -2,6 +2,10 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ $1 ]]
 then
 ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number='$1' OR symbol='$1' OR name='$1'")
+if [[ -z $ELEMENT ]]
+then
+echo "I could not find that element in the database."
+fi
 echo $ELEMENT
 else
 echo "Please provide an element as an argument."
